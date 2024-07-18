bed171ffdc6ba89d29829cad1367894f49136895
Robert J Paisley <56076332+sudo-rpaisley@users.noreply.github.com>
2024-07-18 17:04:33 +0100

    More parts Working

--- app.py
+++ app.py
@@ -6,28 +6,13 @@ from scripts.interfaceTools import *
 
 app = Flask(__name__)
 
-# # Define your technologyMap here:
-# technology_map = {
-#     'Wi-Fi': 'Wireless',
-#     'Ethernet': 'Wired',
-#     'Bluetooth': 'Bluetooth',
-#     'Loopback': 'Loopback',
-#     # Other mappings as necessary
-# }
-
-# def get_adapters():
-#     interfaces = [
-#         {'ifname': 'eth0', 'networkTechnology': 'Ethernet', 'address': '00:1A:2B:3C:4D:5E'},
-#         {'ifname': 'wlan0', 'networkTechnology': 'Wi-Fi', 'address': '00:1A:2B:3C:4D:5F'},
-#         # Add more interfaces as needed
-#     ]
-#     network_technologies = list({iface['networkTechnology'] for iface in interfaces})
-#     return interfaces, network_technologies
+# Fetch network interfaces at the start
+network_interfaces = get_network_interfaces()
+networkTechnologies = {iface.interface_type for iface in network_interfaces}
 
 @app.route('/')
 def index():
-    # interfaces, network_technologies = get_adapters()
-    return render_template('index.html', title='Home') #, interfaces=interfaces, network_technologies=network_technologies, technology_map=technology_map
+    return render_template('index.html', title='Home', networkTechnologies=networkTechnologies, interfaces=network_interfaces)
 
 @app.route('/about')
 def about():
@@ -37,32 +22,27 @@ def about():
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico')
 
-
-
 @app.route('/red-team')
 def red_team():
-    return render_template('red-team.html', title='Red Team') 
+    return render_template('red-team.html', title='Red Team', networkTechnologies=networkTechnologies, interfaces=network_interfaces)
 
-
+@app.route('/<interface_type>')
+def interfaces_by_type(interface_type):
+    interface_type = interface_type.capitalize()
+    filtered_interfaces = [iface for iface in network_interfaces if iface.interface_type.lower() == interface_type.lower()]
+    if filtered_interfaces:
+        return render_template('interface_type.html', title=f'{interface_type} Interfaces', interfaces=filtered_interfaces, networkTechnologies=networkTechnologies)
+    else:
+        return "No interfaces found for this type", 404
 
 @app.route('/<interface_type>/<interface_name>')
 def interface_detail(interface_type, interface_name):
     interface_type = interface_type.lower()
     interface = next((iface for iface in network_interfaces if iface.name == interface_name and iface.interface_type.lower() == interface_type), None)
     if interface:
-        return render_template('interface_detail.html', title=f'{interface_type.capitalize()} - {interface_name}', interface=interface)
+        return render_template('interface_detail.html', title=f'{interface_type.capitalize()} - {interface_name}', interface=interface, networkTechnologies=networkTechnologies)
     else:
         return "Interface not found", 404
-
-@app.route('/<interface_type>')
-def interfaces_by_type(interface_type):
-    interface_type = interface_type.lower()
-    filtered_interfaces = [iface for iface in network_interfaces if iface.interface_type.lower() == interface_type]
-    if filtered_interfaces:
-        return render_template('interface_type.html', title=f'{interface_type.capitalize()} Interfaces', interfaces=filtered_interfaces)
-    else:
-        return "No interfaces found for this type", 404
-
 
 # @app.route('/syn-flood', methods=['POST'])
 # def syn_flood():
@@ -112,13 +92,7 @@ def interfaces_by_type(interface_type):
 #         return jsonify({'status': 'error', 'message': f'WLAN connect error: {str(e)}'})
 
 
-
 if __name__ == '__main__':
-
     host='127.0.0.1'
     port=8080
-    
-    network_interfaces = get_network_interfaces()
     app.run(host=host, port=port, debug=True)
-
-
