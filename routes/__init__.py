deac102f8dfb8e448547e3b325425548edb46a0d
Robert J Paisley <56076332+sudo-rpaisley@users.noreply.github.com>
2026-08-02 23:14:05 +0200

    Register device identification routes

--- routes/__init__.py
+++ routes/__init__.py
@@ -4,6 +4,7 @@ from .minecraft import create_minecraft_blueprint
 from .train_controller import create_train_controller_blueprint
 from .automotive import create_automotive_blueprint
 from .deauth_control import create_deauth_control_blueprint
+from .device_identification import create_device_identification_blueprint
 
 
 def register_blueprints(app, context_provider):
@@ -13,3 +14,4 @@ def register_blueprints(app, context_provider):
     app.register_blueprint(create_train_controller_blueprint(context_provider))
     app.register_blueprint(create_automotive_blueprint(context_provider))
     app.register_blueprint(create_deauth_control_blueprint(context_provider))
+    app.register_blueprint(create_device_identification_blueprint(context_provider))
