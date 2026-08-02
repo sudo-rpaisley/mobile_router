20038bea06bbe8855a0c0e071b7bbc80700793bd
Robert J Paisley <56076332+sudo-rpaisley@users.noreply.github.com>
2026-08-02 11:58:56 +0200

    Add accessible navigation dropdown handling

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,6 +1,70 @@
 $(document).ready(function () {
   $('[data-toggle="tooltip"]').tooltip();
 
+  function closeNavigationDropdowns(exceptToggle) {
+    $('[data-navigation-toggle="dropdown"]').each(function () {
+      const toggle = $(this);
+      if (exceptToggle && toggle.is(exceptToggle)) {
+        return;
+      }
+      toggle.attr('aria-expanded', 'false');
+      toggle.closest('.dropdown').removeClass('show');
+      toggle.siblings('.dropdown-menu').removeClass('show');
+    });
+  }
+
+  function openNavigationDropdown(toggle) {
+    closeNavigationDropdowns(toggle);
+    toggle.attr('aria-expanded', 'true');
+    toggle.closest('.dropdown').addClass('show');
+    toggle.siblings('.dropdown-menu').addClass('show');
+  }
+
+  $(document).on('click', '[data-navigation-toggle="dropdown"]', function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    const toggle = $(this);
+    const isOpen = toggle.attr('aria-expanded') === 'true';
+    if (isOpen) {
+      closeNavigationDropdowns();
+    } else {
+      openNavigationDropdown(toggle);
+    }
+  });
+
+  $(document).on('keydown', '[data-navigation-toggle="dropdown"]', function (event) {
+    if (event.key === 'ArrowDown') {
+      event.preventDefault();
+      const toggle = $(this);
+      openNavigationDropdown(toggle);
+      toggle.siblings('.dropdown-menu')
+        .find('a:visible, button:visible')
+        .first()
+        .trigger('focus');
+    }
+  });
+
+  $(document).on('keydown', '.app-navbar .dropdown-menu', function (event) {
+    if (event.key === 'Escape') {
+      event.preventDefault();
+      const toggle = $(this).siblings('[data-navigation-toggle="dropdown"]');
+      closeNavigationDropdowns();
+      toggle.trigger('focus');
+    }
+  });
+
+  $(document).on('click', '.app-navbar .dropdown-menu a, .app-navbar .dropdown-menu button', function () {
+    closeNavigationDropdowns();
+  });
+
+  $(document).on('click', function () {
+    closeNavigationDropdowns();
+  });
+
+  $('#navbarCollapse').on('hidden.bs.collapse', function () {
+    closeNavigationDropdowns();
+  });
+
   function updateJobIndicator() {
     $.ajax({
       url: '/jobs/status',
